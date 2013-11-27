4d222760067d164fd484772e627317546af7f66a
Brian Schwartz <brian.schwartz@codeandtheory.com>
2013-11-26 23:15:04 -0500

    Added encryption key

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'fValjdXtW45VcOBkSRPEZZrlm7xRa8ny',
 
 	/*
 	|--------------------------------------------------------------------------
